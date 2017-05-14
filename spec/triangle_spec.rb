d3954108564aa79afc0e5782ecf97398fd0cbbee
apprenticexn <apprenticexn@gmail.com>
2017-05-14 17:50:57 +0900

    triangle.rb, triangle_spec.rb を作成

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,1 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
+
+describe Triangle do
+  let(:arg) { '1, 1, 1' }
+  let(:triangle) { Triangle.new(arg.split(' ')) }
+  subject { triangle.which_category? }
+  context 'must be equilateral' do
+    let(:arg) { '2, 3, 4' }
+    it { expect(subject).to eq '不等辺三角形ですね！' }
+  end
+  context 'must be equilateral' do
+    let(:arg) { '2, 2, 1' }
+    it { expect(subject).to eq '二等辺三角形ですね！' }
+  end
+  context 'must be equilateral' do
+    let(:arg) { '1, 1, 1' }
+    it { expect(subject).to eq '正三角形ですね！' }
+  end
+  context 'must be equilateral' do
+    let(:arg) { '1, 2, 3' }
+    it { expect(subject).to eq '三角形じゃないです＞＜' }
+  end
+end
--- triangle.rb
+++ triangle.rb
@@ -1,3 +1,48 @@
-class Triangle
+require 'pry-byebug'
+require 'rb-readline'
 
+class Triangle
+  attr_accessor :sides
+
+  def initialize(arg)
+    @sides = split_input(arg)
+  end
+
+  def initialize_copy(obj)
+    @sides = obj.sides.dup
+  end
+
+  def which_category?
+    return '三角形じゃないです＞＜' unless triangle?
+    return '正三角形ですね！' if equilateral?
+    return '二等辺三角形ですね！' if isosceles?
+    '不等辺三角形ですね！' if scalene?
+  end
+
+  private
+
+  def triangle?
+    max_side = sides.max
+    temp_sides = sides.dup
+    temp_sides.delete_at(sides.index(max_side))
+    max_side < temp_sides[0] + temp_sides[1]
+  end
+
+  def equilateral?
+    sides.uniq.length == 1
+  end
+
+  def isosceles?
+    sides.uniq.length == 2
+  end
+
+  def scalene?
+    sides.uniq.length == 3
+  end
+
+  def split_input(input)
+    input.map { |i| i.sub(',', '').to_f }
+  end
 end
+
+Triangle.new(ARGV).which_category? unless ARGV.empty?
